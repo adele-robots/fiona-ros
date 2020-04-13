965a9049d8dc6d31c5ae69878481ac32425a2ae1
rachsc <raquelsc94@gmail.com>
2020-04-13 18:20:54 +0200

    Upload fiona_talker

--- fiona_pkg/src/fiona_talker.cpp
+++ fiona_pkg/src/fiona_talker.cpp
@@ -19,9 +19,7 @@ int main(int argc, char **argv)
   	int count = 0;
   	while (ros::ok())
   	{
-    	/**
-     	* This is a message object. You stuff it with data, and then publish it.
-     	*/
+    		//This is a message object. We publish a string message.
     		std_msgs::String msg;
 
     		std::stringstream ss;
@@ -30,19 +28,13 @@ int main(int argc, char **argv)
 
     		ROS_INFO("%s", msg.data.c_str());
 
-    		/**
-     		* The publish() function is how you send messages. The parameter
-     		* is the message object. The type of this object must agree with the type
-     		* given as a template parameter to the advertise<>() call, as was done
-     		* in the constructor above.
-     		*/
     		chatter_pub.publish(msg);
 
     		ros::spinOnce();
 
     		loop_rate.sleep();
     		++count;
-  		}
+  	}
 
 
   return 0;
