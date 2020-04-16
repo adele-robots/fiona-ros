d3c2c264b05fd1fce95359614780215c2dc3cd7a
rachsc <raquelsc94@gmail.com>
2020-04-16 18:16:32 +0200

    added test_audiobuffer

---
+++ fiona_pkg/src/fiona_audiobuffer_server.cpp
@@ -0,0 +1,58 @@
+#include <ros/ros.h>
+#include <fiona_pkg/AudioBuffer_srv.h>
+#include <cstring>
+
+class IAudioBuffer {
+public:
+        // All sizes in bytes, not samples of 16 bit words.
+        virtual void queueAudioBuffer(char *buffer, int size) = 0;
+        virtual void dequeueAudioBuffer(char *buffer, int size) = 0;
+};
+
+class DAudioBuffer: public IAudioBuffer
+{ 
+public:
+	void queueAudioBuffer(char *buffer, int size){}
+        void dequeueAudioBuffer(char *buffer, int size){} 
+
+}; 
+
+bool callbackROS(fiona_pkg::AudioBuffer_srv::Request  &req, fiona_pkg::AudioBuffer_srv::Response &res)
+{
+        res.audiobuffer = false;
+        DAudioBuffer myAudioBuffer;
+        if (!strcmp(req.selection.c_str(), "queue"))
+        {
+                myAudioBuffer.queueAudioBuffer((char*)req.buffer.c_str(), req.size);
+                res.audiobuffer = true;
+        }
+        if (!strcmp(req.selection.c_str(), "dequeue"))
+        {
+                myAudioBuffer.dequeueAudioBuffer((char*)req.buffer.c_str(), req.size);
+                res.audiobuffer = true;
+        }
+        else
+                res.audiobuffer = false;
+
+        return true;
+}
+
+
+int main(int argc, char **argv)
+{
+        bool remap_name = true;
+        bool node_name_empty = true;
+        std::map<std::string, std::string> remaps;
+        if (remap_name) remaps["__name"] = "fiona_service";
+        std::string node_name = "server_audiobuffer";
+        if (node_name_empty) node_name = "server_audiobuffer";
+        ros::init(remaps, node_name, (uint32_t)0);
+  
+        ros::NodeHandle n;
+
+        ros::ServiceServer service = n.advertiseService("fiona_service", callbackROS);
+        ROS_INFO("fiona_service ready");
+        ros::spin();
+
+  return 0;
+}
---
+++ fiona_pkg/tests/test_audiobuffer.cpp
@@ -0,0 +1,30 @@
+#include <ros/ros.h>
+#include <ros/service_client.h>
+#include <gtest/gtest.h>
+#include <fiona_pkg/AudioBuffer_srv.h>
+
+std::shared_ptr<ros::NodeHandle> nh;
+
+TEST(TESTSuite, ValidFilename)
+{
+  ros::ServiceClient client = nh->serviceClient<fiona_pkg::AudioBuffer_srv>("fiona_service");
+  bool exists(client.waitForExistence(ros::Duration(1)));
+  EXPECT_TRUE(exists);
+
+  fiona_pkg::AudioBuffer_srv srv;
+  srv.request.selection = "queue";
+  srv.request.buffer ="buffer1";
+  srv.request.size = 16;
+  client.call(srv);
+
+  EXPECT_EQ(true, srv.response.audiobuffer);
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "test_audiobuffer");
+  nh.reset(new ros::NodeHandle);
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
